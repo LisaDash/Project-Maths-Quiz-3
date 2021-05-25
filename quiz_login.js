1bc271860301821030232062cec039d418f09802
LisaDash <77782340+LisaDash@users.noreply.github.com>
2021-05-25 15:30:12 +0530

    Add files via upload

---
+++ quiz_login.js
@@ -0,0 +1,9 @@
+function add_user() {
+    player1_name = document.getElementById("player1_name").value;
+    player2_name = document.getElementById("player2_name").value;
+
+    localStorage.setItem("player1 name", player1_name);
+    localStorage.setItem("player2 name", player2_name);
+
+    window.location = "quiz_page.html";
+}
